6a960ceee165efb3781c4270368e4e5cf1299fe9
shikha8357 <68018323+shikha8357@users.noreply.github.com>
2026-03-23 18:41:53 +0530

    Create DriverFactory.java

---
+++ src/test/java/driver/DriverFactory.java
@@ -0,0 +1,22 @@
+package driver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DriverFactory {
+
+    private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+
+    public static void initDriver() {
+        driver.set(new ChromeDriver());
+    }
+
+    public static WebDriver getDriver() {
+        return driver.get();
+    }
+
+    public static void quitDriver() {
+        driver.get().quit();
+        driver.remove();
+    }
+}
